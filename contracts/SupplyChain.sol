0aeaa6d5c88204a6ba2c2aef77d2453e2508c885
Marc-Antoine Lemaire <lemaima@MacBook-Air-van-Marc-Antoine.local>
2020-10-21 08:32:51 +0200

    commit

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -4,7 +4,8 @@
     https://solidity.readthedocs.io/en/v0.6.12/060-breaking-changes.html
 */
 
-pragma solidity >=0.6.0 <0.7.0;
+// pragma solidity >=0.6.0 <0.7.0;
+ pragma solidity >=0.4.21 <0.6.0;
 
 contract SupplyChain {
 
